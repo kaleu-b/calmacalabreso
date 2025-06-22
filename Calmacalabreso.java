de36cd1868156afe46d79a776396929f4735f9b8
kaleu-b <kaleu.b@aluno.ifsc.edu.br>
2025-06-22 14:43:43 -0300

    Commit inicial, com os arquivos do robô.
    Fazendo a primeira commmit, que adiciona os arquivos necessários do robô.
    
    Algumas alterações já foram feitas no arquivo, com os métodos que iremos trabalhar e algumas mudanças iniciais (imports, etc.)

---
+++ Calmacalabreso.java
@@ -0,0 +1,30 @@
+package calmacalabreso;
+import robocode.*;
+import java.awt.Color;
+import robocode.util.Utils;
+/**
+ * Calmacalabreso - a robot by Kanna, Kaléu, Michel & Rafael
+ */
+public class Calmacalabreso extends AdvancedRobot {
+
+	public void run() {
+	
+		while(true) {
+		
+		}
+	}
+
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+	
+	}
+
+	public void onHitRobot(HitRobotEvent e){
+
+}
+	
+
+	public void onHitWall(HitWallEvent e) {
+	
+	}	
+}
