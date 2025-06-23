fadc3e4830e09a5f780813b35825c26462725a91
mi <michel.p2004@aluno.ifsc.edu.br>
2025-06-23 15:14:07 -0300

    Adicionando funcionalidade de atirar no inimigo, o robo atira pesado se encontrar o oponente na frente.

--- Calmacalabreso.java
+++ Calmacalabreso.java
@@ -25,6 +25,10 @@ public class Calmacalabreso extends AdvancedRobot {
 	
 
 	public void onHitWall(HitWallEvent e) {
-	
+	/*se o inimigo estiver bem na nossa frente, atirar com o máximo de potência.
+como ele está na nossa frente, não precisa ajustar ângulo nem nada. é só atirar com o máximo de potência.
+problema: não pensei ainda numa forma de lidar com o caso de algum inimigo que não está sendo trackeado bater no nosso por trás ou até pela frente.*/
+	if (e.getBearing() > -90 && e.getBearing() <= 90){
+	setFire(10);
 	}	
 }
