bfbde19acac8ea9a43058e4d035f3a3157238342
kannaputadores <151974596+kankanlearnsputers@users.noreply.github.com>
2025-06-23 22:08:34 +0000

    run() e while(true)

--- Calmacalabreso.java
+++ Calmacalabreso.java
@@ -8,8 +8,21 @@ import robocode.util.Utils;
 public class Calmacalabreso extends AdvancedRobot {
 
 	public void run() {
+		
+		setBodyColor(Color.black); //cor do corpo (ou lataria)
+		setRadarColor(Color.red);  //cor do radar
+		setGunColor(Color.black);  //cor da arma
+		setBulletColor(Color.magenta); //cor da bala
+		
+		//faz o radar e arma se moverem independentemente do robô.
+		setAdjustRadarForGunTurn(true);
+    	setAdjustGunForRobotTurn(true);
 	
 		while(true) {
+
+			// gira o radar infinitamente até encontrar um inimigo. depois roda todo o código dentro do evento scanned robot.
+			
+			turnRadarRightRadians(Double.POSITIVE_INFINITY);
 		
 		}
 	}
